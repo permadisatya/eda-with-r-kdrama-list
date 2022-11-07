f9741a50adf3f5db26c9d10f38b55412a1c6b9ad
permadisatya <36924828+permadisatya@users.noreply.github.com>
2022-11-07 16:33:09 +0700

    Kaggle Notebook | EDA of Korean Drama List  | Version 14

--- eda-of-korean-drama-list.r
+++ eda-of-korean-drama-list.r
@@ -1,27 +1,29 @@
 ---
-title: "KDrama EDA"
+title: "EDA of Korean Drama List"
 date: "`r format(Sys.time(), '%B, %Y')`"
 author: "@permadisatya"
+
 output:
-  bookdown::html_document2:
-    theme: paper
-    highlight: zenburn
+  html_document:
     df_print: paged
     toc: true
-    toc_depth: 2
-    toc_float:
-      collapsed: false
-    number_sections: true
+    toc_depth: 3
+    fig_caption: true
+    fig_width: 7
+    fig_height: 4.5
+    theme: paper
+    highlight: tango
     code_folding: hide
 ---
 
 ```{r setup, include=FALSE}
 # setup
-knitr::opts_chunk$set(cache=FALSE)
+knitr::opts_chunk$set(cache = FALSE)
 options(
   scipen = 9999,
   dplyr.summarise.inform = FALSE
 )
+
 # library
 library(bookdown)
 
@@ -30,6 +32,7 @@ library(reshape2)
 library(lubridate)
 
 library(ggplot2)
+library(ggplotify)
 library(gridExtra)
 library(scales)
 library(RColorBrewer)
@@ -37,27 +40,34 @@ library(RColorBrewer)
 
 ---
 
+# How many unique kdrama and actors?
+
 ```{r}
 tbl_actors <- read.csv("../input/korean-drama-list-about-740-unique-dramas/actors.csv")
-
-p1 <- tbl_actors %>% 
-  group_by(actor) %>% 
-  summarise(total_kdrama = n()) %>% 
+p1 <- tbl_actors %>%
+  group_by(actor) %>%
+  summarise(total_kdrama = n()) %>%
   arrange(desc(total_kdrama)) %>% 
-  ggplot(aes(total_kdrama)) +
-  geom_histogram(binwidth = 5)
-p2 <- tbl_actors %>% 
-  group_by(kdrama_name) %>% 
-  summarise(total_actor = n()) %>% 
+  ggplot(aes(x = total_kdrama)) +
+  geom_histogram(binwidth = 2) +
+  labs(y = "Freq") +
+  theme_minimal()
+p2 <- tbl_actors %>%
+  group_by(kdrama_name) %>%
+  summarise(total_actor = n()) %>%
   arrange(desc(total_actor)) %>% 
-  ggplot(aes(total_actor)) +
-  geom_histogram(binwidth = 1)
+  ggplot(aes(x = total_actor)) +
+  geom_histogram(binwidth = 1) +
+  labs(y = "Freq") +
+  theme_minimal()
 p3 <- data.frame(
   var = c("total_kdrama", "total_actors"),
   count = c(length(unique(tbl_actors$kdrama_name)), length(unique(tbl_actors$actor)))
   ) %>% 
   ggplot(aes(x = var, y = count)) +
-  geom_col()
+  geom_col() +
+  labs(y = "Unique count") +
+  theme_minimal()
 grid.arrange(
   arrangeGrob(p3),
   arrangeGrob(p1, p2, ncol = 1),
